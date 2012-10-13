9bb61b03acfcf583eb860b62dcd29788094d689d
xiaoqie <xiaoqie108@gmail.com>
2012-10-13 11:45:07 +0800

    I am trying to boot my OS with grub. But there is some error, I am afraid my host OS Just in case, I upload uncompleted source

--- src/init/main.c
+++ src/init/main.c
@@ -1,17 +1,20 @@
-#include "video.h"
+/*#include "video.h"
 #include "asm/system.h"
-#include "sys_call.h"
+#include "sys_call.h"*/
+#include "multiboot.h"
 
-extern void scheduleInit();
-extern void sys_fillRect(int x, int y, short color);
+/*extern void scheduleInit();
+extern void sys_fillRect(int x, int y, short color);*/
 
-void main() {
-	sys_fillRect(C_SCREEN_WIDTH << 16, C_SCREEN_HEIGHT << 16, RGB(255,255,255));
-	scheduleInit();
-	sti();
-	moveToUserMode();
-	while(1) {
-		drawString(200 | (100 << 16), "init", 0);
-		drawString(200 | (100 << 16), "init", RGB(0,255,0));
+extern int multiboot_magic;
+extern void *multiboot_mbd;
+void start_kernel() {
+	if (multiboot_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
 	}
+
+	/* Print a letter to screen to see everything is working: */
+	unsigned char *videoram = (unsigned char *)0xB8000;
+	videoram[0] = 65; /* character 'A' */
+	videoram[1] = 0x07; /* light grey (7) on black (0). */
+	while(1);
 }
